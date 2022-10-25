57dfd36b6b77484989ed375a25944e0e21395289
Abir Hasan <cse.170201063@gmail.com>
2022-10-25 18:46:01 +0600

    login page created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import "./Login.css";
+import cover from "../../Assets/images/courses-bg.jpg";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>login</h2>
+  return (
+    <div className="login-container">
+      <img className="cover-image" src={cover} alt="" />
+      <div className="form-container d-flex flex-column justify-content-center align-items-center">
+        <div className="login-title">
+          <h2>Login</h2>
         </div>
-    );
+        <Form className="form ms-3 me-3">
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          {/* {error !== null && (
+          <Form.Group className="mb-3">
+            <Form.Text className="text-danger">{error}</Form.Text>
+          </Form.Group>
+        )} */}
+
+          <button type="submit" className="orangeBtn mb-4">
+            Login
+          </button>
+          <div className="d-lg-flex justify-content-around align-items-center mb-3">
+              <Button
+                //   onClick={handleGoogleSignIn}
+                className="mb-3 mb-lg-0"
+                variant="outline-light"
+              >
+                <FaGoogle></FaGoogle> Login with Google
+              </Button>
+              <Button
+                //   onClick={handleGithubSignIn}
+                variant="outline-light"
+              >
+                <FaGithub></FaGithub> Login with GitHub
+              </Button>
+          </div>
+          <div>
+            <p className="text-white">
+              Not Registered? <Link to="/register">Register</Link>
+            </p>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
