ced670a887d5db3e0ac751cf912671fe05e0a11a
Abir Hasan <cse.170201063@gmail.com>
2022-10-25 16:43:04 +0600

    footer done

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FaCopyright } from 'react-icons/fa';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h2>this is footer</h2>
-        </div>
+        <section className='footer-container d-flex justify-content-center align-items-center '>
+            <p className='m-0'><FaCopyright className='text-white'></FaCopyright> <span className='orange-font'>COPYRIGHT</span> 2022 by <span className='orange-font'>T</span>COURSE</p>
+        </section>
     );
 };
 
