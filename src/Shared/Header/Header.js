6d67d0d109b1df0554d16efabcf031cf0c16f982
Abir Hasan <cse.170201063@gmail.com>
2022-10-25 13:01:07 +0600

    nav bar working and  night mode and dark mode on nav done

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Image } from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from '../../Assets/images/online-learning.png'
+import './Header.css'
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Header = () => {
+
+    const [mode, setMode] = useState('dark');
+    const handleMode = (m) => {
+        setMode(m)
+    }
+
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className="header-container">
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          variant={mode}
+          className={`w-100 h-100 ${mode==='dark'?'dark':'light'}`} 
+        >
+          <Container>
+            <Image style={{ height: "40px" }} src={logo}></Image>
+            <Navbar.Brand href="#home">TCOURSE</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">Courses</Nav.Link>
+                <Nav.Link href="#pricing">FAQs</Nav.Link>
+                <Nav.Link href="#pricing">Blogs</Nav.Link>
+              </Nav>
+              <Nav className=" align-items-center">
+                {mode === "dark" ? (
+                  <div
+                    className="text-light me-3"
+                    onClick={() => handleMode("light")}
+                  >
+                    <FaSun></FaSun>
+                  </div>
+                ) : (
+                  <div className="me-3" onClick={() => handleMode("dark")}>
+                    <FaMoon></FaMoon>
+                  </div>
+                )}
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  Dank memes
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
 };
 
