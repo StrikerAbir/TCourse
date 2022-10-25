5e692b79cd87e643d60f77a81d10d21f00183551
Abir Hasan <cse.170201063@gmail.com>
2022-10-25 21:26:15 +0600

    blogs part done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import cover from "../../Assets/images/courses-bg.jpg";
+import { useLoaderData } from 'react-router-dom';
+import Blog from './Blog/Blog';
 import './Blogs.css'
 const Blogs = () => {
-    return (
-      <div className="container-all">
-            <img className="cover-image" src={cover} alt="" />
-            <div className="body-container text-white">
+    const blogs = useLoaderData();
 
+    return (
+      <div className="container-all text-white">
+          <div className="container mt-5">
+              <h2 className="text-center fw-bold mb-4 orange-border p-2">
+                BLogs
+              </h2>
+            <div className="">
+              {blogs.map((blog) => (
+                <Blog key={Math.random()} blog={blog}></Blog>
+              ))}
             </div>
+          </div>
       </div>
     );
 };
