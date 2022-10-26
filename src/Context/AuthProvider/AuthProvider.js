2c22f3529911e271a6dbd4c8e2430637353713e7
Abir Hasan <cse.170201063@gmail.com>
2022-10-26 21:41:16 +0600

    some commits

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -18,39 +18,45 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     
+    // login with google and github
     const providerLogin = (provider) => {
         setLoading(true);
         return signInWithPopup(auth, provider);
   };
 
+    // logout 
   const logOut = () => {
     setLoading(true);
     return signOut(auth);
   };
 
+    // creating user
   const createUser = (email, password) => {
       setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+    // user signin
   const signIn = (email, password) => {
        setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
     };
     
+    // update user profile 
      const updateUserProfile = (profile) => {
        return updateProfile(auth.currentUser, profile);
     };
     
+    // email varification
     const verifyEmail = () => {
       return sendEmailVerification(auth.currentUser);
     };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        if (currentUser === null || currentUser.emailVerified) {
-          setUser(currentUser);
-      }
+        setUser(currentUser);
+    //     if (currentUser === null || currentUser.emailVerified) {
+    //   }
       setLoading(false);
     });
     return () => {
